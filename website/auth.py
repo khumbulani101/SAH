d9cf658310bbc9631fb0eaaf0ffa11b94660045e
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-03-06 22:24:20 +0200

    I am define my auther routes for authentication in the application.

--- website/auth.py
+++ website/auth.py
@@ -2,3 +2,14 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def login():
+    return "<p>logout</p>"
+
+@auth.route('/sign-up')
+def login():
+    return "<p>Sing Up</p>"
