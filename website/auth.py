742fb4d5a2a676956e44213e2712a821c505ef1e
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-03-07 22:07:56 +0200

    The change in Approach where I flash messages to the user is much better for having a user-friendly system.

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -20,15 +20,15 @@ def sign_up():
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            pass
+            flash('Email must be greater than 4 charecters.', category='error')
         elif len(firstName) < 2:
-            pass
+            flash('First name must be greater than 2 charecters.', category='error')
         elif password1 != password2:
-            pass
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            pass
+            flash('Passwords must be at least 7 characters.', category='error')
         else:
-            # add user to database
+            flash('Account created successfully!', category='success')
             pass
 
     return render_template("sign_up.html")
