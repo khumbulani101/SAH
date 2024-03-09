0d903ba0cd03f7eb373630c703645a9a693ec2c8
Nkululeko Sibeko <112828662+nkululekosibeko@users.noreply.github.com>
2024-03-09 10:14:26 +0200

    Database Models
    Restructuring everything.

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-
-from website import models
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
@@ -21,9 +21,19 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
